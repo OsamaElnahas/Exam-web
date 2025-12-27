f8656500ba695fd4eeb70c8edf4ed21de493ac70
Youssef Kassab <yousefkassab75@gmail.com>
2025-12-27 22:43:11 +0200

    Exam Result Page

---
+++ start-exam/script.js
@@ -0,0 +1,15 @@
+var startBtn = document.querySelector("#start-exam");
+var loadingSpinner = document.querySelector("#loading-spinner");
+var btnContent = document.querySelector("#btn-content");
+
+startBtn.addEventListener("click", function () {
+  startBtn.disabled = true;
+  startBtn.style.cursor = "not-allowed";
+
+  btnContent.style.display = "none";
+  loadingSpinner.style.display = "block";
+
+  setTimeout(function () {
+    window.location.replace("../exam-page/exam.html");
+  }, 1500);
+});
