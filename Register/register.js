352220ba7afa25d6f075dccb181bae7ddb2a71f1
Youssef Kassab <yousefkassab75@gmail.com>
2025-12-29 20:41:18 +0200

    Handled the array of objects in localstorage

--- Register/register.js
+++ Register/register.js
@@ -1,108 +1,171 @@
 var fNameRegex = /^[A-Za-z]{2,30}$/;
 var lNameRegex = /^[A-Za-z]{2,30}$/;
 var emailRegex = /^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.(com|net|org)$/;
-var PasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+var PasswordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 var fName = document.getElementById("fName");
 var lName = document.getElementById("lName");
-var emailInput = document.getElementById('email');
-var passwordInput = document.getElementById('password');
-var confirmPasswordInput = document.getElementById('confirm-password');
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password");
+var confirmPasswordInput = document.getElementById("confirm-password");
 
-var fNameErrMsg = document.getElementById('fNameErrMsg')
-var lNameErrMsg = document.getElementById('lNameErrMsg')
-var emailErrMsg = document.getElementById('emailErrMsg')
-var passwordErrMsg = document.getElementById('passwordErrMsg')
-var confirmPasswordErrMsg = document.getElementById('confirmPasswordErrMsg')
+var fNameErrMsg = document.getElementById("fNameErrMsg");
+var lNameErrMsg = document.getElementById("lNameErrMsg");
+var emailErrMsg = document.getElementById("emailErrMsg");
+var passwordErrMsg = document.getElementById("passwordErrMsg");
+var confirmPasswordErrMsg = document.getElementById("confirmPasswordErrMsg");
 
-var submitBtn = document.getElementById('submitBtn');
+var submitBtn = document.getElementById("submitBtn");
 
-var registerForm = document.querySelector('form');
-
-
+var registerForm = document.querySelector("form");
 
 function validateField(input, regex, errorMsg, emptyMsg, invalidMsg) {
-    if (input.value.trim() === "") {
-        errorMsg.textContent = emptyMsg;
-        errorMsg.style.display = "block";
+  if (input.value.trim() === "") {
+    errorMsg.textContent = emptyMsg;
+    errorMsg.style.display = "block";
 
-        return false;
-    } else if (!regex.test(input.value)) {
-        errorMsg.textContent = invalidMsg;
-        errorMsg.style.display = "block";
-        return false;
-    } else {
-        errorMsg.textContent = "";
-        errorMsg.style.display = "none";
-        return true;
-    }
-
+    return false;
+  } else if (!regex.test(input.value)) {
+    errorMsg.textContent = invalidMsg;
+    errorMsg.style.display = "block";
+    return false;
+  } else {
+    errorMsg.textContent = "";
+    errorMsg.style.display = "none";
+    return true;
+  }
 }
 
-
-fName.addEventListener("blur", () => {
-    validateField(fName, fNameRegex, fNameErrMsg, "First Name is required", "First Name must be letters and (2–30 chars)");
+fName.addEventListener("blur", function () {
+  validateField(
+    fName,
+    fNameRegex,
+    fNameErrMsg,
+    "First Name is required",
+    "First Name must be letters and (2–30 chars)"
+  );
 });
 
-lName.addEventListener("blur", () => {
-    validateField(lName, lNameRegex, lNameErrMsg, "Last Name is required", "Last Name must be letters and (2–30 chars)");
+lName.addEventListener("blur", function () {
+  validateField(
+    lName,
+    lNameRegex,
+    lNameErrMsg,
+    "Last Name is required",
+    "Last Name must be letters and (2–30 chars)"
+  );
 });
-emailInput.addEventListener("blur", () => {
-    validateField(emailInput, emailRegex, emailErrMsg, "Email is required", "Please enter a valid email address");
+emailInput.addEventListener("blur", function () {
+  validateField(
+    emailInput,
+    emailRegex,
+    emailErrMsg,
+    "Email is required",
+    "Please enter a valid email address"
+  );
 });
-passwordInput.addEventListener("blur", () => {
-    validateField(passwordInput, PasswordRegex, passwordErrMsg, "Password is required", "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character");
+passwordInput.addEventListener("blur", function () {
+  validateField(
+    passwordInput,
+    PasswordRegex,
+    passwordErrMsg,
+    "Password is required",
+    "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
+  );
 });
 
 var isValidConfirmPassword = false;
 
-confirmPasswordInput.addEventListener("blur", () => {
-    if (confirmPasswordInput.value.trim() === "") {
-        confirmPasswordErrMsg.textContent = "Confirm Password is required";
-        confirmPasswordErrMsg.style.display = "block";
+confirmPasswordInput.addEventListener("blur", function () {
+  if (confirmPasswordInput.value.trim() === "") {
+    confirmPasswordErrMsg.textContent = "Confirm Password is required";
+    confirmPasswordErrMsg.style.display = "block";
+  } else if (confirmPasswordInput.value !== passwordInput.value) {
+    confirmPasswordErrMsg.textContent = "Passwords do not match";
+    confirmPasswordErrMsg.style.display = "block";
+  } else {
+    confirmPasswordErrMsg.textContent = "";
+    confirmPasswordErrMsg.style.display = "none";
+    isValidConfirmPassword = true;
+  }
+});
 
-    } else if (confirmPasswordInput.value !== passwordInput.value) {
-        confirmPasswordErrMsg.textContent = "Passwords do not match";
-        confirmPasswordErrMsg.style.display = "block";
+registerForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var isFnameValid = validateField(
+    fName,
+    fNameRegex,
+    fNameErrMsg,
+    "First Name is required",
+    "First Name must be letters and (2–30 chars)"
+  );
+  var isLnameValid = validateField(
+    lName,
+    lNameRegex,
+    lNameErrMsg,
+    "Last Name is required",
+    "Last Name must be letters and (2–30 chars)"
+  );
+  var isEmailValid = validateField(
+    emailInput,
+    emailRegex,
+    emailErrMsg,
+    "Email is required",
+    "Please enter a valid email address"
+  );
+  var isPasswordValid = validateField(
+    passwordInput,
+    PasswordRegex,
+    passwordErrMsg,
+    "Password is required",
+    "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
+  );
+  if (
+    isFnameValid &&
+    isLnameValid &&
+    isEmailValid &&
+    isPasswordValid &&
+    isValidConfirmPassword
+  ) {
+    var users = localStorage.getItem("users");
+    if (users) {
+      users = JSON.parse(users);
     } else {
-        confirmPasswordErrMsg.textContent = "";
-        confirmPasswordErrMsg.style.display = "none";
-        isValidConfirmPassword = true;
-    }
-});
-
-
-
-registerForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var isFnameValid = validateField(fName, fNameRegex, fNameErrMsg, "First Name is required", "First Name must be letters and (2–30 chars)");
-    var isLnameValid = validateField(lName, lNameRegex, lNameErrMsg, "Last Name is required", "Last Name must be letters and (2–30 chars)");
-    var isEmailValid = validateField(emailInput, emailRegex, emailErrMsg, "Email is required", "Please enter a valid email address");
-    var isPasswordValid = validateField(passwordInput, PasswordRegex, passwordErrMsg, "Password is required", "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character");
-    if (isFnameValid && isLnameValid && isEmailValid && isPasswordValid && isValidConfirmPassword) {
-        console.log("Form submitted successfully!");
-        var userData = {
-            firstName: fName.value,
-            lastName: lName.value,
-            email: emailInput.value,
-            password: passwordInput.value
-        };
-        submitBtn.textContent = "Submitting...";
-        submitBtn.disabled = true;
-        submitBtn.style.cursor = "not-allowed";
-        setTimeout(function() {
-            window.location.replace("../Login/login.html");
-        }, 2000);
-        localStorage.setItem('userData', JSON.stringify(userData));
-        console.log(userData);
+      users = [];
     }
 
+    //checking on previously registered emails
+    var emailExists = false;
+
+    users.forEach(function (user) {
+      if (user.email === emailInput.value) {
+        emailExists = true;
+      }
+    });
+
+    if (emailExists) {
+      emailErrMsg.textContent = "Email already registered";
+      emailErrMsg.style.display = "block";
+      return;
+    }
+
+    var userData = {
+      firstName: fName.value,
+      lastName: lName.value,
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    users.push(userData);
+    localStorage.setItem("users", JSON.stringify(users));
+
+    submitBtn.textContent = "Submitting...";
+    submitBtn.disabled = true;
+    submitBtn.style.cursor = "not-allowed";
+
+    setTimeout(function () {
+      window.location.replace("../Login/login.html");
+    }, 1500);
+  }
 });
-
-if (localStorage.getItem('userData')) {
-    window.location.replace("../Login/login.html");
-}
-
-// lName.addEventListener("change", () => {
-//     validateField(lName, lNameRegex, lNameErrMsg, "Last Name is required", "Last Name must be letters and (2–30 chars)");
-// });
--- login/login.js
+++ login/login.js
@@ -6,37 +6,59 @@ var loginBtn = document.getElementById("loginBtn");
 var emailErrMsg = document.getElementById("emailErrMsg");
 var passwordErrMsg = document.getElementById("passwordErrMsg");
 
+if (localStorage.getItem("isLoggedIn")) {
+  window.location.replace("../start-exam/index.html");
+}
+
 function validateEmailPassword(email, password) {
+  emailErrMsg.textContent = "";
+  passwordErrMsg.textContent = "";
+
   if (!email || !password) {
     emailErrMsg.textContent = "Email is required.";
     passwordErrMsg.textContent = "Password is required.";
     return false;
-  } else if (
-    email !== JSON.parse(localStorage.getItem("userData")).email ||
-    password !== JSON.parse(localStorage.getItem("userData")).password
-  ) {
-    passwordErrMsg.textContent = "Incorrect email or password.";
-    return false;
+  }
+
+  var users = localStorage.getItem("users");
+  if (users) {
+    users = JSON.parse(users);
   } else {
+    users = [];
+  }
+
+  var foundUser = null;
+
+  users.forEach(function (user) {
+    if (user.email === email && user.password === password) {
+      foundUser = user;
+    }
+  });
+
+  if (foundUser) {
     emailErrMsg.textContent = "";
     passwordErrMsg.textContent = "";
+
+    localStorage.setItem("currentUser", JSON.stringify(foundUser));
+    localStorage.setItem("isLoggedIn", "true");
+
     return true;
+  } else {
+    passwordErrMsg.textContent = "Incorrect email or password";
+    return false;
   }
 }
 
 loginBtn.addEventListener("click", function (e) {
   e.preventDefault();
+
   if (validateEmailPassword(emailInput.value, passwordInput.value)) {
     loginBtn.textContent = "Logging in...";
-    localStorage.setItem("isLoggedIn", "true");
-    setTimeout(function () {
-      window.location.replace("../start-exam/index.html");
-    }, 2000);
-
     loginBtn.disabled = true;
     loginBtn.style.cursor = "not-allowed";
+
+    setTimeout(function () {
+      window.location.replace("../start-exam/index.html");
+    }, 1500);
   }
 });
-if (localStorage.getItem("isLoggedIn")) {
-  window.location.replace("../start-exam/index.html");
-}
