7d4921cab13b495394d61ca93f4fd0dca9667c4c
osama elnahhas <osamaelnahas43@gmail.com>
2026-01-05 12:13:11 +0200

    handling routing in all pages

--- start-exam/script.js
+++ start-exam/script.js
@@ -1,30 +1,3 @@
-if (!localStorage.getItem("currentUserId")) {
-    window.location.replace("../login/login.html");
-}
-
-
-
-var users = JSON.parse(localStorage.getItem("users")) || [];
-var currentUserId = Number(localStorage.getItem("currentUserId"));
-var currentUser = null;
-
-
-for (var i = 0; i < users.length; i++) {
-    if (users[i].id === currentUserId) {
-        currentUser = users[i];
-        break;
-    }
-}
-
-
-if (currentUser.exam.submitted) {
-    window.location.replace("../exam-result/result.html");
-}
-
-
-if (!currentUser) {
-    window.location.replace("../login/login.html");
-}
 var startBtn = document.querySelector("#start-exam");
 var loadingSpinner = document.querySelector("#loading-spinner");
 var btnContent = document.querySelector("#btn-content");
